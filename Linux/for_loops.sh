6865ded62f15f502f25224f21c65fef72e04374a
dwpatt95 <dwpatt95@gmail.com>
2022-04-02 15:31:54 -0500

    First commit

---
+++ Linux/for_loops.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# variables
+fav_states=('minnesota' 'georgia' 'arizona' 'florida' 'tennesee')
+
+for states in ${fav_states[@]}
+do
+if [ $fav_states == 'hawaii' ]
+then
+echo "Hawaii is the best!"
+else
+echo "im not fond of Hawaii"
+fi
+done
+
+
---
+++ Linux/update.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+apt update -y
+
+apt upgrade -y
+
+apt full-upgrade --purge -y
+
+
